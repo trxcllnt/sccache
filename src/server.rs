e7090d951e1828de14601e86c383f5ad1daa2000
ptaylor <paul.e.taylor@me.com>
2025-02-20 20:19:31 -0800

    use nproc * 2 worker threads in the client

--- src/server.rs
+++ src/server.rs
@@ -448,10 +448,7 @@ pub fn start_server(config: &Config, addr: &crate::net::SocketAddr) -> Result<()
     let client = Client::new();
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(std::cmp::max(
-            20,
-            2 * std::thread::available_parallelism().unwrap().get(),
-        ))
+        .worker_threads(2 * std::thread::available_parallelism().unwrap().get())
         .build()?;
     let pool = runtime.handle().clone();
     let dist_client = DistClientContainer::new(config, &pool);
