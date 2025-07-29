6fc27acf01bb72c5849203fc1dc014f1a2377fc2
ptaylor <paul.e.taylor@me.com>
2025-07-29 16:51:46 -0700

    add step argument

--- tests/benchmarks/cuda/test.sh
+++ tests/benchmarks/cuda/test.sh
@@ -12,6 +12,7 @@ Options:
   -b|--build-type <debug|release>  If sccache not on PATH, use this build dir's binary
   -c|--clear-cache                 \`rm /tmp/sccache_* ~/.cache/sccache\` before building
   -i|--iterations <num>            Number of total builds to run (default: 100)
+  -s|--step       <num>            Iteration step (default: 1)
   -j|--jobs       <num>            Number of builds to run concurrently (default: \`nproc\`, but can be higher)
   -t|--threads    <num>            Number of CUDA device architectures to compile in parallel (default: up to 20)
 EOF
@@ -19,10 +20,11 @@ EOF
 
 BUILD_TYPE=debug
 JOBS="$(grep -cP 'processor\s+:' /proc/cpuinfo)"
+STEP=1
 ITERATIONS=100
 THREADS=20
 
-eval set -- "$(getopt -n "$0" -o b:chi:j:t: --long build-type:,clear-cache,help,iterations:,jobs:,threads -- "$@")"
+eval set -- "$(getopt -n "$0" -o b:chi:j:s:t: --long build-type:,clear-cache,help,iterations:,jobs:,step:,threads -- "$@")"
 
 while true; do
     case "$1" in
@@ -46,6 +48,10 @@ while true; do
             ;;
         -t|--threads)
             THREADS="$2";
+            shift 2;
+            ;;
+        -s|--step)
+            STEP="$2";
             shift 2;
             ;;
         -h|--help)
@@ -73,19 +79,19 @@ if ! command -v sccache >/dev/null 2>&1; then
     export PATH="$(realpath -m "$(pwd)/../../../target/$(uname -m)-unknown-linux-musl/${BUILD_TYPE:-debug}"):$PATH"
 fi
 
-time seq 1 "${ITERATIONS}" | xargs -n1 -P"${JOBS}" bash -c "$(cat <<EOF
+time seq "$((1 - (STEP % 2)))" "$STEP" "$((ITERATIONS - (STEP % 2)))" | xargs -n1 -P"${JOBS}" bash -c "$(cat <<"EOF"
 CMD=(sccache nvcc
+    -t=$0
     -arch=all
-    -t=$THREADS
-    -DNONCE=\$0
+    -DNONCE=$1
     -I ./include
     -c ./src/simpleP2P.cu
-    -o ./build/simpleP2P.\$0.cu.o
+    -o ./build/simpleP2P.$1.cu.o
     -MD
-    -MT ./build/simpleP2P.\$0.cu.o
-    -MF ./build/simpleP2P.\$0.cu.o.d
+    -MT ./build/simpleP2P.$1.cu.o
+    -MF ./build/simpleP2P.$1.cu.o.d
 )
-echo "\$0: \${CMD[@]}";
-"\${CMD[@]}" || exit 255
+echo "$1: ${CMD[@]}";
+"${CMD[@]}" || exit 255
 EOF
-)"
+)" "$THREADS"
